e5cb7ee863d9b7fa7abc2a16df7a5dcd1e173209
txq <158013884@qq.com>
2020-11-11 20:33:11 +0800

    1

--- src/beans/Classroom.java
+++ src/beans/Classroom.java
@@ -1,5 +1,13 @@
 package beans;
 
 public class Classroom {
+	 String roomname;
 
+	public String getRoomname() {
+		return roomname;
+	}
+
+	public void setRoomname(String roomname) {
+		this.roomname = roomname;
+	}
 }
