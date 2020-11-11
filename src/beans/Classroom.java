2b95139df15659125f44d49f718bc01179c72f6b
txq <158013884@qq.com>
2020-11-11 20:31:28 +0800

    1

--- src/beans/Classroom.java
+++ src/beans/Classroom.java
@@ -1,5 +1,14 @@
 package beans;
 
 public class Classroom {
+	private int roomid;
 
+	public int getRoomid() {
+		return roomid;
+	}
+
+	public void setRoomid(int roomid) {
+		this.roomid = roomid;
+	}
+	
 }
